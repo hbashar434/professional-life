83e77dc7c0fc38751c7586ff85114da90a19a958
hbashar434 <h.bashar434@gmail.com>
2023-04-10 02:17:41 +0600

    up to banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
-    <div>
-      <div>
-        <h2>
-          One Step Closer To Your <span>Dream Job</span>
-        </h2>
+    <div className="grid md:grid-cols-2 gap-8  items-center mt-16">
+      <div className="order-1 md:order-none">
+        <div className=" text-5xl font-bold grid gap-3">
+          <h1>One Step</h1>
+          <h1>Closer To Your</h1>
+          <h1 className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+            Dream Job
+          </h1>
+        </div>
+        <p className="mt-8">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <Link>
+          <button className="mt-8 btn border-none bg-gradient-to-r from-indigo-500 via-indigo-500 to-pink-400">
+            Get Started
+          </button>
+        </Link>
       </div>
+      <figure>
+        <img src="banner.png" alt="banner-image" />
+      </figure>
     </div>
   );
 };
