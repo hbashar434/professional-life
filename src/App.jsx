5fbdd050e2087294e3354f32c1370af15573b41c
hbashar434 <h.bashar434@gmail.com>
2023-04-10 01:24:34 +0600

    up to react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Professional Life</h1>
+    <div className="lg:mx-24">
+      <Header></Header>
+      <div className="min-h-[calc(100vh-64px)]">
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Statistic from "./components/Statistic/Statistic";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blog from "./components/Blog/Blog";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistic",
+        element: <Statistic></Statistic>,
+      },
+      {
+        path: "/applied-jobs",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
