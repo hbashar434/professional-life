e9a58c1af1af082fbbd9b8555895d5c47cbdc6b8
hbashar434 <h.bashar434@gmail.com>
2023-04-11 03:05:17 +0600

    fixing seeAllJobs btn

--- src/components/FeaturedJob/FeaturedJob.jsx
+++ src/components/FeaturedJob/FeaturedJob.jsx
@@ -1,9 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { FeaturedContext } from "../Home/Home";
 import FeaturedCard from "../FeaturedCard/FeaturedCard";
 
 const FeaturedJob = () => {
+  const [jobData, setJobData] = useState([]);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
   const [data] = useContext(FeaturedContext);
+  useEffect(() => {
+    setJobData(data.slice(0, 4));
+  }, []);
+
   return (
     <div className="mt-20 mb-20">
       <h1 className=" text-4xl font-bold mb-4 text-center">Featured Jobs</h1>
@@ -12,15 +18,17 @@ const FeaturedJob = () => {
         need. Its your future
       </p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {/* @TODO: backTO HOME ERROR */}
-        {data
-          ? data.map((job) => (
-              <FeaturedCard key={job.id} job={job}></FeaturedCard>
-            ))
-          : ""}
-        {/* {data.map((job) => (
+        {jobData.map((job) => (
           <FeaturedCard key={job.id} job={job}></FeaturedCard>
-        ))} */}
+        ))}
+      </div>
+      <div
+        className={`text-center mt-5 ${isButtonClicked ? "hidden" : ""}`}
+        onClick={() => setIsButtonClicked(true)}
+      >
+        <button className="my-btn-2" onClick={() => setJobData(data)}>
+          See All Jobs
+        </button>
       </div>
     </div>
   );
